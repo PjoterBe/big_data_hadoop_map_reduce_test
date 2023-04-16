42de0390f0c18484697f2e90264e90597cd7f9eb
PjoterBe <pietasharp@gmail.com>
2023-04-16 18:58:49 +0200

    1st JOB

--- 01_intro/01_simple_map_reduce.py
+++ 01_intro/01_simple_map_reduce.py
@@ -1,6 +1,12 @@
 from mrjob.job import MRJob
 
-class MRWordCountMRJob):
+class MRWordCount(MRJob):
 
     def mapper(self, _, line):
-        yield  'chars', len(line)
+        yield  'chars', len(line) #liczy liczbe znaków w pliku
+
+    def reducer(self, key, values):
+        yield key, sum(values) #;iczy wszystkie znaki pliku
+
+if __name__ == '__main__':
+    MRWordCount.run()
